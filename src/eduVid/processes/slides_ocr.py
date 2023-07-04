7c9ea5e52f2d83bb5fdf41669260749870439f86
miloSK1 <126353688+miloSK1@users.noreply.github.com>
2023-07-05 00:05:43 +0200

    Update slides_ocr.py - class

--- src/eduVid/processes/slides_ocr.py
+++ src/eduVid/processes/slides_ocr.py
@@ -3,50 +3,47 @@ from PIL import Image
 from pytesseract import pytesseract
 import os
 
-# TODO
-#  Define paths
-path_to_tesseract = r'!your_path!\tesseract.exe'
-path_to_images = r''
+class SlideOCR:
+    def __init__(self, tesseract_path, images_folder, output_file, slide_separator='=== SLIDE ===', language='deu'):
+        self.tesseract_path = tesseract_path
+        self.images_folder = images_folder
+        self.output_file = output_file
+        self.slide_separator = slide_separator
+        self.language = language
 
-# Link tesseract
-pytesseract.tesseract_cmd = path_to_tesseract
+    def ocr_text_from_slides(self):
+        # Set Tesseract OCR executable path
+        pytesseract.tesseract_cmd = self.tesseract_path
 
-# Create the merged output text file
-merged_output_file = 'merged_output.txt'
+        # Get the file names in the directory
+        file_texts = []
 
-# Define the separator between slide texts
-slide_separator = '=== SLIDE ==='
+        for root, dirs, file_names in os.walk(self.images_folder):
+            # Iterate over each file name in the folder
+            for file_name in file_names:
+                # Construct the full path to the image file
+                image_path = os.path.join(root, file_name)
 
-# Set OCR language to German
-ocr_language = 'deu'
+                # Read the image using OpenCV
+                img = cv2.imread(image_path, 0)  # Read as grayscale
 
-# Get the file names in the directory
-file_texts = []
+                # Apply thresholding to the image
+                _, thresholded_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-for root, dirs, file_names in os.walk(path_to_images):
-    # Iterate over each file name in the folder
-    for file_name in file_names:
-        # Construct the full path to the image file
-        image_path = os.path.join(root, file_name)
+                # Create a PIL Image object from the thresholded image
+                pil_img = Image.fromarray(thresholded_img)
 
-        # Read the image using OpenCV
-        img = cv2.imread(image_path, 0)  # Read as grayscale
+                # Extract text from the thresholded image with the specified language
+                text = pytesseract.image_to_string(pil_img, lang=self.language)
 
-        # Apply thresholding to the image
-        _, thresholded_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                # Append the extracted text to the list
+                file_texts.append(text)
 
-        # Create a PIL Image object from the thresholded image
-        pil_img = Image.fromarray(thresholded_img)
+        # Join the file texts using the slide separator
+        merged_text = '\n\n'.join([f"{self.slide_separator}\n{text}" for text in file_texts])
 
-        # Extract text from the thresholded image with German language
-        text = pytesseract.image_to_string(pil_img, lang=ocr_language)
+        # Save the merged output as a single text file
+        with open(self.output_file, 'w', encoding='utf-8') as file:
+            file.write(merged_text)
 
-        # Append the extracted text to the list
-        file_texts.append(text)
-
-# Join the file texts using the slide separator
-merged_text = '\n\n'.join([f"{slide_separator}\n{text}" for text in file_texts])
-
-# Save the merged output as a single text file
-with open(merged_output_file, 'w', encoding='utf-8') as file:
-    file.write(merged_text)
+        print("Step 2 --> OCR : done")
