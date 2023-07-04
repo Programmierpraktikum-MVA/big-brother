50a81323b1a1a640d0f526e13dbaf2245b3e8c59
miloSK1 <126353688+miloSK1@users.noreply.github.com>
2023-07-05 00:03:19 +0200

    Update speech2text.py - class

--- src/eduVid/processes/speech2text.py
+++ src/eduVid/processes/speech2text.py
@@ -1,33 +1,32 @@
-# Importieren der benötigten Module
-from pvrecorder import PvRecorder
-import wave, struct 
-import whisper
+# Importiere die Module
 import moviepy.editor as mp
-import requests
 import os
 
-# Laden des Whisper-Modells. Wir können "tiny = 32x Geschwindigkeit" ausprobieren; base und medium sind besser, aber langsamer
-tiny = whisper.load_model("tiny")
-fp16=False
+class VideoScript:
+    def __init__(self, model):
+        self.model = model
 
-# Angeben der URL zur Videodatei
-video_url = 'https://video.isis.tu-berlin.de/isisvideo/file?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImV4dCJ9.eyJ0eXBlIjoidmlkZW8iLCJoYXNoIjoiZGEyODk3M2U1YzFkNGM2MzYyOWNjMDI5NmYzNDM0MDFhZGQ3YzI0NWZkN2QwNzU1ZWIyNzUzNWQ4YjVkM2NmZiIsImZpbGVfZXh0IjoiLm1wNCIsImlzcyI6ImV4dCIsInJvbGUiOjAsInJpZCI6Ijg3LjEzNS4xNzYuMTIzIiwiZXhwIjoxNjg1NjY0MDAwfQ.baSnFQWvNmgX2dz1SvsUQcpOp3Ovs2joPtRRKdgswRM'
-audio_path = 'audio_from_video.wav'
+    def transcribe_video(self, video_path, output_dir):
 
-# Herunterladen der Videodatei an einen temporären Ort
-temp_path = 'temp_video.mp4'
-r = requests.get(video_url)
-with open(temp_path, 'wb') as f:
-    f.write(r.content)
+        video = mp.VideoFileClip(video_path)
+        audio = video.audio
 
-# Extrahieren des Audios aus dem Video
-video_clip = mp.VideoFileClip(temp_path)
-audio_clip = video_clip.audio
-audio_clip.write_audiofile(audio_path)
+        os.makedirs(output_dir, exist_ok=True)
 
-# Transkribieren des Audios mit dem Whisper-Modell
-result=tiny.transcribe(audio_path)
-print(result["text"]) 
+        audio_file_path = os.path.join(output_dir, "audio.wav")
+        audio.write_audiofile(audio_file_path)
 
-# Löschen der temporären Videodatei
-os.remove(temp_path)
+        video.close()
+
+        result = self.model.transcribe(audio_file_path)
+
+        transcript_file_path = os.path.join(output_dir, "transcript.txt")
+        with open(transcript_file_path, "w") as f:
+            f.write(result["text"])
+
+        transcript_form_file_path = os.path.join(output_dir, "transcript_form.txt")
+        with open(transcript_form_file_path, "w") as f:
+            sentences = result["text"].split(".")
+            for sentence in sentences:
+                sentence = sentence + "."
+                f.write(sentence + "\n")
